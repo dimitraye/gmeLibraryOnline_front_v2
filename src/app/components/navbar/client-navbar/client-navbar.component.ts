c83ecbdf0c524637dd1fc8916e18ed473bd6e532
Dimitri MOMPEROUSSE <momperousse.dimitri.pro@gmail.com>
2025-03-05 16:10:49 +0100

    FRONT : login and register page visuals are done.

--- src/app/components/navbar/client-navbar/client-navbar.component.ts
+++ src/app/components/navbar/client-navbar/client-navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-client-navbar',
@@ -8,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrl: './client-navbar.component.scss'
 })
 export class ClientNavbarComponent {
+
+  constructor(private router: Router) {}
+
+  navigateToHome(): void {
+    this.router.navigate(['/']);
+  }
+
+  navigateToProfile(): void {
+    this.router.navigate(['/profile']);
+  }
+  navigateToUserStats(): void {
+    this.router.navigate(['/user-stats']);
+  }
+  navigateToMyGames(): void {
+    this.router.navigate(['/game-list']);
+  }
+  navigateToAddGames(): void {
+    this.router.navigate(['/game-form']);
+  }
+  navigateToDiscover(): void {
+    this.router.navigate(['/discover']);
+  }
+
+  navigateToUserHome(): void {
+    this.router.navigate(['/home']);
+  }
 
 }
